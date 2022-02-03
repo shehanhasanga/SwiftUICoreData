15a34a86418e31dd02d39efd5212e15c382bbf74
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-03 09:48:26 +0530

    Initial Commit

---
+++ SampleCoredata/SampleCoredataApp.swift
@@ -0,0 +1,20 @@
+//
+//  SampleCoredataApp.swift
+//  SampleCoredata
+//
+//  Created by shehan karunarathna on 2022-02-03.
+//
+
+import SwiftUI
+
+@main
+struct SampleCoredataApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
